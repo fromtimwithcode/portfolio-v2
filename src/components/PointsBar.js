f72b27404a3fb5a9a5ce7cf50c3c4f0e10dc7512
Tim Marchant <fromtimwithcode@gmail.com>
2022-09-16 02:06:08 -0500

    points bar without functionality completed

---
+++ src/components/PointsBar.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import styled from 'styled-components'
+import coin from './../images/coin.png'
+
+function PointsBar() {
+    return (
+        <Container>
+            <Bar>
+                <CoinInfo>
+                    <StyledCoin src={coin} alt="coin" />
+                    <Paragraph>0 points</Paragraph>
+                </CoinInfo>
+                <Right>
+                    <SpendButton style={{ marginRight: '5px' }}>
+                        spend
+                    </SpendButton>
+                    <SpendButton>?</SpendButton>
+                </Right>
+            </Bar>
+        </Container>
+    )
+}
+
+const SpendButton = styled.div`
+    border: 1px solid #fff;
+    border-radius: 3px;
+    display: flex;
+    padding: 2px 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-size: 0.8rem;
+    letter-spacing: 1px;
+    color: #000;
+    background-color: #fff;
+    &:hover {
+        cursor: pointer;
+        border: 1px solid #000;
+    }
+`
+
+const Right = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-right: 10px;
+`
+
+const Paragraph = styled.p`
+    padding-left: 8px;
+    font-size: 1.2rem;
+    font-weight: 600;
+    color: #fff;
+`
+
+const StyledCoin = styled.img`
+    width: 16%;
+`
+
+const CoinInfo = styled.div`
+    display: flex;
+    align-items: center;
+    padding-left: 10px;
+`
+
+const Bar = styled.div`
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 3px solid #fff;
+    border-left: 3px solid #fff;
+    border-right: 3px solid #fff;
+    border-top-left-radius: 8px;
+    border-top-right-radius: 8px;
+    bottom: 0;
+    width: 300px;
+    height: 40px;
+    background-color: #bb86fc;
+    @media (min-width: 501px) {
+        right: 40px;
+    }
+`
+
+const Container = styled.div`
+    box-sizing: border-box;
+    font-family: Helvetica;
+    position: fixed;
+    display: flex;
+    justify-content: flex-end;
+    bottom: 0;
+    width: 100%;
+    max-width: 1400px;
+    height: 40px;
+    @media (max-width: 500px) {
+        justify-content: center;
+    }
+`
+
+export default PointsBar
