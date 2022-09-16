06a9d8c6b4bfdb0c2426a3f4ee2cf6f2150892c3
Tim Marchant <fromtimwithcode@gmail.com>
2022-09-16 02:07:04 -0500

    hide points bar until system is ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import NavBar from './components/NavBar'
 import Hero from './components/Hero'
 import Portfolio from './components/Portfolio'
 import Footer from './components/Footer'
-import PointsBar from './components/PointsBar'
+// import PointsBar from './components/PointsBar'
 
 function App() {
     return (
@@ -13,7 +13,8 @@ function App() {
             <Hero />
             <Portfolio />
             <Footer />
-            <PointsBar />
+            {/* Hide points bar until system is ready */}
+            {/* <PointsBar /> */}
         </Container>
     )
 }
