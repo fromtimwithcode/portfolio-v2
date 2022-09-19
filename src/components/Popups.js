7800d9f4435f8ed506b34723b6f26903972c1fe4
Tim Marchant <fromtimwithcode@gmail.com>
2022-09-19 00:42:06 -0500

    when 'spend' is clicked, alert users that functionality is coming soon

--- src/components/Popups.js
+++ src/components/Popups.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 import EarnedCoinsPopup from '../features/popups/EarnedCoinsPopup'
+import SpendPopup from './../features/popups/SpendPopup'
 import InfoPopup from '../features/popups/InfoPopup'
 
 function Popups() {
     const showEarnedCoinsPopUp = useSelector(
         (state) => state.popups.earnedCoinsInfo
     )
+    const showSpendInfoPopUp = useSelector((state) => state.popups.spendInfo)
     const showGameInfoPopUp = useSelector((state) => state.popups.gameInfo)
     return (
         <>
             {showEarnedCoinsPopUp && <EarnedCoinsPopup />}
+            {showSpendInfoPopUp && <SpendPopup />}
             {showGameInfoPopUp && <InfoPopup />}
         </>
     )
